c673b83b37b24ce13964ea6a7dd8079712930592
Stef Van den Bruel <stef.van.den.bruel@tenforce.com>
2018-10-16 14:40:55 +0200

    Script clean up and added an example usage scene

---
+++ Assets/Scripts/ExampleUsage.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleUsage : MonoBehaviour
+{
+    public float SecondsPerShot = 2f;
+    private float _currentTime;
+	
+	void Update () {
+        // Timer to shoot projectiles in random direction
+	    if (_currentTime >= SecondsPerShot)
+	    {
+            // Teleport projectile
+	        StartCoroutine(TeleportObject());
+
+	        // Reset timer
+	        _currentTime = 0f;
+        }
+	    else
+	    {
+            // Add to the timer
+	        _currentTime += Time.deltaTime;
+	    }
+	}
+
+    private IEnumerator TeleportObject()
+    {
+        var projectiles = new List<Transform>();
+        for (var i = 0; i < 20; i++)
+        {
+            var randomPosition = new Vector3(Random.Range(transform.position.x - 10, transform.position.x + 10),
+                Random.Range(transform.position.y - 10, transform.position.y + 10), 0);
+
+            // Get an existing projectile with name 'Projectile', else create one
+            var projectile = ObjectPool.GetCustom<Transform>(f => f.name == "Projectile");
+            if (projectile != null)
+            {
+                // Change position
+                projectile.transform.position = randomPosition;
+            }
+            else
+            {
+                // Create new sphere projectile
+                projectile = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+
+                // Interact with projectile
+                projectile.name = "Projectile";
+                projectile.position = randomPosition;
+            }
+
+            projectiles.Add(projectile);
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        // Pool the projectiles again because we no longer need them
+        foreach (var projectile in projectiles)
+            ObjectPool.Add(projectile);
+    }
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class ObjectPool
+{
+    /// <summary>
+    /// The pool collection.
+    /// </summary>
+    private static readonly Dictionary<string, List<Component>> Pool = new Dictionary<string, List<Component>>();
+
+    /// <summary>
+    /// Clear's the entire pool of objects, must be called on new scene load.
+    /// </summary>
+    public static void Clear()
+    {
+        Pool.Clear();
+    }
+
+    /// <summary>
+    /// Returns the amount of objects in the pool of this given type.
+    /// </summary>
+    /// <returns></returns>
+    public static int GetAmountInPool<T>() where T : Component
+    {
+        List<Component> poolableObjects;
+        return Pool.TryGetValue(typeof(T).Name, out poolableObjects) ? poolableObjects.Count : 0;
+    }
+
+    /// <summary>
+    /// Add poolable obj to the pool.
+    /// </summary>
+    /// <param name="poolableObj"></param>
+    public static void Add(Component poolableObj)
+    {
+        List<Component> poolableObjects;
+        var key = poolableObj.GetType().Name;
+        if (!Pool.TryGetValue(key, out poolableObjects))
+        {
+            // Add to pool and deactivate enemy.
+            Pool.Add(key, new List<Component> {poolableObj});
+            poolableObj.gameObject.SetActive(false);
+        }
+        else
+        {
+            poolableObjects.Add(poolableObj);
+            poolableObj.gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Get available poolable obj from the pool converted to its concrete type.
+    /// </summary>
+    /// <returns></returns>
+    public static T Get<T>() where T : Component
+    {
+        List<Component> poolableObjects;
+        var key = typeof(T).Name;
+
+        if (Pool.TryGetValue(key, out poolableObjects))
+        {
+            var poolableObj = poolableObjects.First();
+            // Get existing enemy data
+            poolableObjects.Remove(poolableObj);
+            poolableObj.gameObject.SetActive(true);
+            if (poolableObjects.Count == 0)
+                Pool.Remove(key);
+            return GetConcreteType<T>(poolableObj);
+        }
+
+        return default(T);
+    }
+
+    /// <summary>
+    /// Get an object in the pool by a custom lambda.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="criteria"></param>
+    /// <returns></returns>
+    public static T GetCustom<T>(Func<T, bool> criteria) where T : Component
+    {
+        List<Component> poolableObjects;
+        var key = typeof(T).Name;
+
+        if (!Pool.TryGetValue(key, out poolableObjects)) return default(T);
+        var customObjects = poolableObjects.Where(f => criteria.Invoke((T) f));
+        var poolableObj = customObjects.FirstOrDefault();
+        if (poolableObj == null) return default(T);
+
+        // Get existing enemy data
+        poolableObjects.Remove(poolableObj);
+        poolableObj.gameObject.SetActive(true);
+        if (poolableObjects.Count == 0)
+            Pool.Remove(key);
+
+        return GetConcreteType<T>(poolableObj);
+    }
+
+    /// <summary>
+    /// Returns the concrete type of this PoolableType.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    private static T GetConcreteType<T>(Component obj) where T : Component
+    {
+        try
+        {
+            return (T) Convert.ChangeType(obj, obj.GetType());
+        }
+        catch (InvalidCastException)
+        {
+            return default(T);
+        }
+    }
+}
